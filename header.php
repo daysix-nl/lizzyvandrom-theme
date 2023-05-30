ad3339793305499760d5f465ec3d0d203b361748
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-05-30 21:19:14 +0200

    WIP

--- header.php
+++ header.php
@@ -11,10 +11,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo( 'name' ); ?> | <?php the_title(); ?></title>
-    <link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">
+    
     <?php wp_head(); ?>
 </head>
 
