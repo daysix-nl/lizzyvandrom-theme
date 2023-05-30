3c4d00d524cc4967e89a8e02dba315298bed877d
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-05-30 21:01:46 +0200

    WIP

--- page-templates/home.php
+++ page-templates/home.php
@@ -8,14 +8,9 @@
  
 <main>
     <section class="h-[522px] max-h-[unset] md:max-h-[640px] lg:max-h-[unset] md:h-screen bg-center bg-cover relative" style="background-image: url('/wp-content/themes/doove-theme/img/local/image1.png');">
-        <video class="w-full h-full object-cover absolute top-0 right-0 -z-10" autoplay="" loop="" muted="" playsinline="">
-        <source src="https://player.vimeo.com/video/831162641?h=c385849a37&muted=1&autoplay=1&dnt=1&loop=1&transparent=0&background=1&app_id=122963">
-        </video>
-        <iframe class="w-full h-full object-cover absolute top-0 right-0 -z-10" src="https://player.vimeo.com/video/831162641?h=c385849a37&muted=1&autoplay=1&dnt=1&loop=1&transparent=0&background=1&app_id=122963" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-        
-        
-        <div class="container flex h-full items-center pt-[41px] md:pt-[35.5px] lg:pt-[34px]">
-            <h1 class="text-22 leading-30 max-w-[206px] md:text-40 md:leading-46 md:max-w-[373px] lg:text-61 lg:leading-70 lg:max-w-[569px] xl:text-70 xl:leading-80 xl:max-w-[653px] font-satoshi text-black font-semibold">Lizzy van Drom</h1>
+        <iframe class="w-full h-full object-cover absolute top-0 right-0 left-0 bottom-0 -z-10" src="https://player.vimeo.com/video/831162641?h=c385849a37&muted=1&autoplay=1&dnt=1&loop=1&transparent=0&background=1&app_id=122963" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        <div class="container flex h-full items-center justify-center">
+            <h1 class="text-22 leading-30 max-w-[206px] md:text-40 md:leading-46 md:max-w-[373px] lg:text-61 lg:leading-70 lg:max-w-[569px] xl:text-70 xl:leading-80 xl:max-w-[653px] font-satoshi text-white font-semibold">Lizzy van Drom</h1>
         </div>
     </section>
     <?php the_content(); ?>
