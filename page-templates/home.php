cd7b0fd0861e113a3639608373b4ea7c0f6096b0
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-05-30 20:51:27 +0200

    WIP

--- page-templates/home.php
+++ page-templates/home.php
@@ -8,7 +8,11 @@
  
 <main>
     <section class="h-[522px] max-h-[unset] md:max-h-[640px] lg:max-h-[unset] md:h-screen bg-center bg-cover relative" style="background-image: url('/wp-content/themes/doove-theme/img/local/image1.png');">
-        <video src="https://player.vimeo.com/video/831162641?h=c385849a37&muted=1&autoplay=1&dnt=1&loop=1&transparent=0&background=1&app_id=122963" class="absolute object-center object-cover top-0 left-0 right-0 bottom-0"></video>
+        <video class="w-full h-full object-cover absolute top-0 right-0 -z-10" autoplay="" loop="" muted="" playsinline="">
+        <source src="https://player.vimeo.com/video/831162641?h=c385849a37&muted=1&autoplay=1&dnt=1&loop=1&transparent=0&background=1&app_id=122963">
+        </video>
+        
+        
         <div class="container flex h-full items-center pt-[41px] md:pt-[35.5px] lg:pt-[34px]">
             <h1 class="text-22 leading-30 max-w-[206px] md:text-40 md:leading-46 md:max-w-[373px] lg:text-61 lg:leading-70 lg:max-w-[569px] xl:text-70 xl:leading-80 xl:max-w-[653px] font-satoshi text-black font-semibold">Lizzy van Drom</h1>
         </div>
